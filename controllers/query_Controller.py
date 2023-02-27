cfac44c3a606635128da3e39a1438a180fea73fe
ariso-sac <86800159+ariso-sac@users.noreply.github.com>
2023-02-28 01:08:22 +0530

    first_push
    Basic CodeBase

---
+++ controllers/query_Controller.py
@@ -0,0 +1,103 @@
+from models import db
+from flask_restful import Resource
+from flask import request
+from models.queries import Query
+
+
+def GetDeck(id):
+    d = Query.query.filter_by(id=id).first()
+    return d
+
+
+def GetAllDecks():
+    d = Query.query.all()
+    return d
+
+# def GetCardsbyDeck(id):
+#     d=Cards.query.filter_by(query_id=id).all()
+#     return d
+
+# def GetScoresbyDeck(id):
+#     d=Scores.query.filter_by(query_id=id).all()
+#     return d
+
+
+class Query_Others(Resource):
+    def get(query_id):
+        g = GetDeck(query_id)
+        return g.GetDict(), 200
+
+    def delete(query_id):
+        g = GetDeck(query_id)
+        if g == None:
+            return {"Error": "Not Found"}, 404
+        db.session.delete(g)
+        db.session.commit()
+        return g.GetDict(), 200
+
+    def put(query_id):
+        g = GetDeck(query_id)
+        if request.content_type == 'application/json':
+            issue = request.json['issue']
+            solution = request.json['solution']
+            created_by = request.json['created_by']
+            answered_by = request.json['answered_by']
+            upvotes = 0
+        else:
+            issue = request.form['issue']
+            solution = request.form['solution']
+            created_by = request.form['created_by']
+            answered_by = request.form['answered_by']
+            upvotes = 0
+        if g == None:
+            return {"Error": "NotFound"}, 404
+        if issue == None:
+            return {"Error": "Incorecct Parameter"}, 400
+        g.issue = issue
+        g.solution = solution
+        g.created_by = created_by
+        g.answered_by = answered_by
+        g.upvotes = upvotes
+        db.session.commit()
+        g = GetDeck(query_id)
+        return g.GetDict(), 200
+
+
+def creteDeck(issue, solution, created_by, answered_by, upvotes):
+    d = Query(
+        issue=issue,
+        solution=solution,
+        created_by=created_by,
+        answered_by=answered_by,
+        upvotes=upvotes,
+    )
+    db.session.add(d)
+    db.session.commit()
+    return d
+
+
+class Query_Create(Resource):
+    def post(self):
+        if request.content_type == 'application/json':
+            issue = request.json['issue']
+            solution = request.json['solution']
+            created_by = request.json['created_by']
+            answered_by = request.json['answered_by']
+            upvotes = 0
+        else:
+            issue = request.form['issue']
+            solution = request.form['solution']
+            created_by = request.form['created_by']
+            answered_by = request.form['answered_by']
+            upvotes = 0
+        if issue == None:
+            return {"Error": "Incorecct Parameter"}, 400
+        d = creteDeck(issue, solution, created_by, answered_by, upvotes)
+        return d.GetDict(), 201
+
+    def get(self):
+        g = GetAllDecks()
+        a = []
+        for x in g:
+            a.append(x.GetDict())
+        return a
